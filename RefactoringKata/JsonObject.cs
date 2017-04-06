156d85b01e68426aae6b437e41d387486c435963
eason.lin62 <eason.lin@titansoft.com.sg>
2017-04-06 23:31:08 +0800

    Move to JsonObject.cs

---
+++ RefactoringKata/JsonObject.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace RefactoringKata
+{
+    public class JsonObject
+    {
+        public StringBuilder Sb;
+
+        public JsonObject()
+        {
+            Sb = new StringBuilder();
+            Sb.Append("{");
+        }
+
+        public void AddProperty(string name, string value)
+        {
+            Sb.Append(GetJsonProperty(name, value));
+        }
+
+        public void AddProperty(string name, double value)
+        {
+            Sb.Append(GetJsonProperty(name, value));
+        }
+
+
+        private string GetJsonProperty(string name, string value)
+        {
+            return string.Format("\"{0}\": \"{1}\", ", name, value);
+        }
+
+        private string GetJsonProperty(string name, double value)
+        {
+            return string.Format("\"{0}\": {1}, ", name, value);
+        }
+
+        public string GetJsonString()
+        {
+            Sb.Remove(Sb.Length - 2, 2);
+            Sb.Append("}, ");
+            return Sb.ToString();
+        }
+
+        public void AddArray<T>(string name, List<T> values) where T : IJsonObject
+        {
+            Sb.Append(string.Format("\"{0}\": [", name));
+            foreach (var value in values)
+            {
+                Sb.Append(value.GetJsonString());
+            }
+
+            if (values.Count > 0)
+            {
+                Sb.Remove(Sb.Length - 2, 2);
+            }
+            Sb.Append("], ");
+        }
+    }
+}
--- RefactoringKata/OrdersWriter.cs
+++ RefactoringKata/OrdersWriter.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Text;
+﻿using System.Text;
 
 namespace RefactoringKata
 {
@@ -28,60 +27,6 @@ namespace RefactoringKata
             }
 
             return sb.Append("]}").ToString();
-        }
-    }
-
-    public class JsonObject
-    {
-        public StringBuilder Sb;
-
-        public JsonObject()
-        {
-            Sb = new StringBuilder();
-            Sb.Append("{");
-        }
-
-        public void AddProperty(string name, string value)
-        {
-            Sb.Append(GetJsonProperty(name, value));
-        }
-
-        public void AddProperty(string name, double value)
-        {
-            Sb.Append(GetJsonProperty(name, value));
-        }
-
-
-        private string GetJsonProperty(string name, string value)
-        {
-            return string.Format("\"{0}\": \"{1}\", ", name, value);
-        }
-
-        private string GetJsonProperty(string name, double value)
-        {
-            return string.Format("\"{0}\": {1}, ", name, value);
-        }
-
-        public string GetJsonString()
-        {
-            Sb.Remove(Sb.Length - 2, 2);
-            Sb.Append("}, ");
-            return Sb.ToString();
-        }
-
-        public void AddArray<T>(string name, List<T> values) where T : IJsonObject
-        {
-            Sb.Append(string.Format("\"{0}\": [", name));
-            foreach (var value in values)
-            {
-                Sb.Append(value.GetJsonString());
-            }
-
-            if (values.Count > 0)
-            {
-                Sb.Remove(Sb.Length - 2, 2);
-            }
-            Sb.Append("], ");
         }
     }
 }
