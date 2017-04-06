60ab3366b34a34f2c64575ffbab30ef3c0dfcf06
eason.lin62 <eason.lin@titansoft.com.sg>
2017-04-07 01:13:55 +0800

    Add enum Color, replace switch case.

---
+++ RefactoringKata/Color.cs
@@ -0,0 +1,17 @@
+﻿namespace RefactoringKata
+{
+    public enum Color
+    {
+        [TypeText("no color")]
+        NoColor = 0,
+
+        [TypeText("blue")]
+        Blue,
+
+        [TypeText("red")]
+        Red,
+
+        [TypeText("yellow")]
+        Yellow
+    }
+}
--- RefactoringKata/Product.cs
+++ RefactoringKata/Product.cs
@@ -3,7 +3,7 @@
     public class Product : IJsonString
     {
         public string Code { get; set; }
-        public int Color { get; set; }
+        public Color Color { get; set; }
         public Size Size { get; set; }
         public double Price { get; set; }
         public string Currency { get; set; }
@@ -11,7 +11,7 @@
         public Product(string code, int color, int size, double price, string currency)
         {
             Code = code;
-            Color = color;
+            Color = (Color)color;
             Size = (Size)size;
             Price = price;
             Currency = currency;
@@ -21,7 +21,7 @@
         {
             var jsonObject = new JsonObject();
             jsonObject.AddProperty("code", Code);
-            jsonObject.AddProperty("color", GetColorFor());
+            jsonObject.AddProperty("color", Color.GetTypeText());
             if (Size != Size.SizeNotApplicable)
             {
                 jsonObject.AddProperty("size", Size.GetTypeText());
@@ -30,21 +30,6 @@
             jsonObject.AddProperty("currency", Currency);
 
             return jsonObject.GetJsonString();
-        }
-
-        private string GetColorFor()
-        {
-            switch (Color)
-            {
-                case 1:
-                    return "blue";
-                case 2:
-                    return "red";
-                case 3:
-                    return "yellow";
-                default:
-                    return "no color";
-            }
         }
     }
 }
